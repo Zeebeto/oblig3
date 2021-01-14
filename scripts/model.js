ed96cfe8d0e795300cbfbe852142f6fb3ddf00e0
Zeebeto <72790252+Zeebeto@users.noreply.github.com>
2021-01-14 14:48:14 +0100

    Initial commit

---
+++ scripts/model.js
@@ -0,0 +1,14 @@
+const model = [
+    {
+        cards: {
+            id: 1,
+            name: "questing beast",
+            want: 4,
+            have: 0,
+            price: 13,
+            buyLink: "https://www.cardmarket.com/en/Magic/Products/Singles/Throne-of-Eldraine/Questing-Beast",
+            picLink: "https://static.cardmarket.com/img/32c208d02b46456211f793f17c87ed48/items/1/ELD/399999.jpg",
+            done: false,
+                },
+    },
+];
